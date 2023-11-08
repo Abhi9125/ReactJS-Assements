5e1b438ef5b6b83e28df4af321b4eb89ddba0440
Abhi9125 <abhi.singh3132@gmail.com>
2023-11-09 01:11:41 +0530

    Task1 - (c) Completed

--- Coding_Assement/app.js
+++ Coding_Assement/app.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 
 const TaskThree = () => (
   <div className="title">
-    <h1>I'm heading 1.</h1>
-    <h2>I'm heading 2</h2>
-    <h3>I'm Heading 3</h3>
+    <h1 className="Heading1">I'm heading 1.</h1>
+    <h2 className="Heading2">I'm heading 2</h2>
+    <h3 className="Heading3">I'm Heading 3</h3>
   </div>
 );
 // const TaskTwo = <h1>I'm Heading 1.</h1>;
